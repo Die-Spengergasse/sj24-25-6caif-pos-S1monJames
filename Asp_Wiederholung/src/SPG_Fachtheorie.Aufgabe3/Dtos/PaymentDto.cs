50d8c63c3f8c4fcc4345f6c32017d9185d293b71
Simon Stjepanovic <STJ19722@spengergasse.at>
2025-03-12 18:36:21 +0100

    03_post_requests_Stjepanovic

--- Asp_Wiederholung/src/SPG_Fachtheorie.Aufgabe3/Dtos/PaymentDto.cs
+++ Asp_Wiederholung/src/SPG_Fachtheorie.Aufgabe3/Dtos/PaymentDto.cs
@@ -1,7 +1,6 @@
 ﻿namespace SPG_Fachtheorie.Aufgabe3.Dtos
 {
-    public record PaymentDto(
-        int id, string employeeFirstName, string employeeLastName,
-        int cashDeskNumber, string paymentType, int totalAmount);
-    
+    public record PaymentDto(int id, string employeeFirstName,
+        string employeeLastName, int cashDeskNumber,
+        string paymentType, int totalAmount);
 }
